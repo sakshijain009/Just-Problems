d2e903bd638e480a10a26abbfbd1d60baac20987
Sakshi Jain <62885857+sakshijain009@users.noreply.github.com>
2021-06-22 15:08:20 +0530

    Coin Change Permutations and Combinations

--- DP/coinChange1.java
+++ DP/coinChange1.java
@@ -10,14 +10,28 @@ class Testclass {
         }
         int tar = s.nextInt();
 
-        int[] count = new int[tar+1];
-        count[0] = 1;
+        // TOTAL COMBINATIONS TO PAY THE AMOUNT
+        int[] countC = new int[tar+1];
+        countC[0] = 1;
 
         for (int i = 0; i < coins.length; i++) {
-            for (int j = coins[i]; j < count.length; j++) {
-                count[j] += count[j-coins[i]];
+            for (int j = coins[i]; j < countC.length; j++) {
+                countC[j] += countC[j-coins[i]];
             }
         }
-        System.out.println(count[count.length-1]);
+        System.out.println("Combinations "+countC[tar]);
+
+        // TOTAL PERMUTATIONS TO PAY THE AMOUNT
+        int[] countP = new int[tar+1];
+        countP[0] = 1;
+
+        for (int i = 1; i < countP.length; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j]<=i){
+                    countP[i]+=countP[i-coins[j]];
+                }
+            }
+        }
+        System.out.println("Permutations "+countP[tar]);
     }
 }
