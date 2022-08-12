0777f7f09fb59efdb0df34d8e34aeb89782402ca
Sakshi Jain <62885857+sakshijain009@users.noreply.github.com>
2022-08-12 11:47:11 +0530

    HasPath in Graph

--- graph/createGraph.java
+++ graph/createGraph.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Graph {
+class Scratch {
     static class Edge {
         int src;
         int nbr;
@@ -14,35 +14,52 @@ class Graph {
         }
     }
 
-    public static void main(String[] args) {
-        int vces = 7; // 0, 1, 2, 3, 4, 5, 6, 7
-        ArrayList<Edge>[] graph = new ArrayList<Edge>[7];
-
-        for (int i = 0; i < vces; i++) {
-            graph[i] = new ArrayList<Edge>();
+    // If path exists from Vertex 1 to Vertex 2
+    public static boolean hasPath(ArrayList<ArrayList<Edge>> graph, int src, int dest, boolean[] visited){
+        if(src == dest) return true;
+        visited[src] = true;
+        for(Edge edg : graph.get(src)){
+            if(visited[edg.nbr] == false){
+                boolean hasNbrPath = hasPath(graph, edg.nbr, dest, visited);
+                if (hasNbrPath == true) return true;
+            }
         }
 
-        graph[0].add(new Edge(0,3,40));
-        graph[0].add(new Edge(0,1,10));
+        return false;
+    }
 
-        graph[1].add(new Edge(1,0,10));
-        graph[1].add(new Edge(1,2,10));
+    public static void main(String[] args) {
+        int vces = 7; // 0, 1, 2, 3, 4, 5, 6, 7
+        ArrayList<ArrayList<Edge>> graph = new ArrayList<>(7);
 
-        graph[2].add(new Edge(2,3,10));
-        graph[2].add(new Edge(2,1,10));
+        for (int i = 0; i < vces; i++) {
+            graph.add(new ArrayList<Edge>());
+        }
 
-        graph[3].add(new Edge(3,0,40));
-        graph[3].add(new Edge(3,2,10));
-        graph[3].add(new Edge(3,4,2));
+        graph.get(0).add(new Edge(0,3,40));
+        graph.get(0).add(new Edge(0,1,10));
 
-        graph[4].add(new Edge(4,3,2));
-        graph[4].add(new Edge(4,5,3));
-        graph[4].add(new Edge(4,6,3));
+        graph.get(1).add(new Edge(1,0,10));
+        graph.get(1).add(new Edge(1,2,10));
 
-        graph[5].add(new Edge(5,4,3));
-        graph[5].add(new Edge(5,6,3));
+        graph.get(2).add(new Edge(2,3,10));
+        graph.get(2).add(new Edge(2,1,10));
 
-        graph[6].add(new Edge(6,5,3));
-        graph[6].add(new Edge(6,4,8));
+        graph.get(3).add(new Edge(3,0,40));
+        graph.get(3).add(new Edge(3,2,10));
+        graph.get(3).add(new Edge(3,4,2));
+
+        graph.get(4).add(new Edge(4,3,2));
+        graph.get(4).add(new Edge(4,5,3));
+        graph.get(4).add(new Edge(4,6,3));
+
+        graph.get(5).add(new Edge(5,4,3));
+        graph.get(5).add(new Edge(5,6,3));
+
+        graph.get(6).add(new Edge(6,5,3));
+        graph.get(6).add(new Edge(6,4,8));
+
+        //System.out.println(graph);
+        System.out.println(hasPath(graph,0,6, new boolean[vces]));
     }
 }
