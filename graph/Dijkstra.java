855447aa489ba4646f9c05f8153b6f1bc8d863ff
Sakshi Jain <62885857+sakshijain009@users.noreply.github.com>
2022-08-20 16:08:27 +0530

    Min Weight Path

---
+++ graph/Dijkstra.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+class Scratch {
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int v; // Vertex
+        String psf; // Path So Far
+        int wsf; // Wait so Far
+
+        Pair(int v, String psf, int wsf){
+            this.v = v;
+            this.psf = psf;
+            this.wsf = wsf;
+        }
+
+        public int compareTo(Pair o){
+            return this.wsf - o.wsf;
+        }
+    }
+
+    // Main function
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for(int i = 0; i < vtces; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for(int i = 0; i < edges; i++){
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            int wt = Integer.parseInt(parts[2]);
+            graph[v1].add(new Edge(v1, v2, wt));
+            graph[v2].add(new Edge(v2, v1, wt));
+        }
+
+        int src = Integer.parseInt(br.readLine());
+
+        /*
+            EXAMPLE INPUT :
+            7
+            8
+            0 3 40
+            0 1 10
+            1 2 10
+            2 3 10
+            3 4 2 
+            4 5 3
+            5 6 3
+            4 6 8
+            0
+         */
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(src,src+"",0));
+        boolean[] visited = new boolean[vtces]; // Stores level of the vertex
+
+        while (pq.size() > 0){
+            Pair rem = pq.remove();
+
+            if (visited[rem.v] == true){
+                continue;
+            }
+
+            visited[rem.v] = true;
+            System.out.println(rem.v + " via " + rem.psf + " for weight : " + rem.wsf);
+
+            for (Edge e: graph[rem.v]){
+                if (visited[e.nbr] == false){
+                    pq.add(new Pair(e.nbr, rem.psf + e.nbr, rem.wsf + e.wt));
+                }
+            }
+        }
+    }
+}
